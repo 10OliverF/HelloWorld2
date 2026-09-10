e2867cd52380efc2d5b1ff858233b8ef214da145
10OliverF <ofolkersen@gmail.com>
2026-09-10 08:44:00 +0200

    Add unit test to validate expected vs actual values in HelloWorld2ApplicationTests

--- src/test/java/dk/oliver/helloworld2/HelloWorld2ApplicationTests.java
+++ src/test/java/dk/oliver/helloworld2/HelloWorld2ApplicationTests.java
@@ -3,11 +3,20 @@ package dk.oliver.helloworld2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class HelloWorld2ApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void TestFail() {
+        int expected = 5;
+        int actual = 3;
+        assertEquals(expected, actual, "The actual value does not match the expected value");
     }
 
 }
